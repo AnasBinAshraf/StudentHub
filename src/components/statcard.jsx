d62de5ee8e7c61e2c30625b0056d94bd63e2ccd1
AnasBinAshraf <anasbinashraf2000@gmail.com>
2026-09-07 03:13:33 +0530

    improved allover css styling

--- src/components/statcard.jsx
+++ src/components/statcard.jsx
@@ -1,11 +1,33 @@
 function StatCard({ title, value, subtitle }) {
+
+  let icon = "📋";
+
+  if (title === "Upcoming") {
+    icon = "⏰";
+  }
+
+  if (title === "Notes") {
+    icon = "📝";
+  }
+
+  if (title === "Productivity") {
+    icon = "📈";
+  }
+
   return (
     <div className="stat-card">
-      <span>{title}</span>
+
+      <div className="stat-top">
+        <span>{title}</span>
+        <div className="stat-icon">
+          {icon}
+        </div>
+      </div>
 
       <strong>{value}</strong>
 
       <small>{subtitle}</small>
+
     </div>
   );
 }
